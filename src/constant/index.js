62622b59aa09b07578949107b4afbb4344c11dd3
王少魁 <wangshaokui@ssit-xm.com.cn>
2019-03-21 23:19:40 +0800

    项目初始化

---
+++ src/constant/index.js
@@ -0,0 +1,17 @@
+export const navbarList = [
+  {
+    "content": "主页",
+    "info" : [require("../assets/icon/home/home_normal.png"),require("../assets/icon/home/home_show.png")],
+    "componentName" : "home"
+  },
+  {
+    "content": "类别",
+    "info" : [require("../assets/icon/type/type_normal.png"),require("../assets/icon/type/type_show.png")],
+    "componentName" : "type"
+  },
+  {
+    "content": "个人",
+    "info" : [require("../assets/icon/my/my_normal.png"),require("../assets/icon/my/my_show.png")],
+    "componentName" : "my"
+  }
+]
