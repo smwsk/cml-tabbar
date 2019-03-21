ecdf446ef83017c3bd265b7960bc4b00e1c17d41
王少魁 <wangshaokui@ssit-xm.com.cn>
2019-03-21 23:52:09 +0800

    添加docs文档

--- chameleon.config.js
+++ chameleon.config.js
@@ -1,6 +1,6 @@
 
 // 设置静态资源的线上路径
-const publicPath = '//github.com/smwsk/cml-navbar/tree/master/dist';
+const publicPath = '//github.com/smwsk/cml-navbar/';
 // 设置api请求前缀
 const apiPrefix = 'https://api.chameleon.com';
 
